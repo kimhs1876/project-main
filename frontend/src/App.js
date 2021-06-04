fd09323d87d415270dd738e0b95a1c673c51ca97
kimhs1876 <kimhs1876>
2021-06-04 14:21:46 +0900

    make develop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,34 @@
 import React from 'react'
+import { BrowserRouter as Router, Link } from "react-router-dom"
+import styled from 'styled-components';
+import Counter from './counter/counter'
 
 const App = () => {
-  return (<>
+  return (<Container>
+  <Element>
   <nav>
-    <Link to="/">Home</Link>
-    <Link to="/">Blog</Link>
-    <Link to="/">About Me</Link>
+    <Router>
+      <Link to="/">Home</Link><br/>
+      <Link to="/">Blog</Link><br/>
+      <Link to="/">About Me</Link><br/>
+      <Link to="">Go to Google</Link><br/>
+    </Router>
   </nav>
-  <h1>1번째 크기 헤드라인</h1>
-  <h2>2번째 크기 헤드라인</h2>
-  <h3>3번째 크기 헤드라인</h3>
-  <h4>4번째 크기 헤드라인</h4>
-  <h5>5번째 크기 헤드라인</h5>
-  <p>문단은 p로 쓰세요. p는 아마도 Paragraph의 앞글자를 따온 것이겠죠?</p>
-  <Link to="/">Go to google</Link>
-  <hr/>
-    <img src="/" width="500px">
-  </>)
-}
 
+  <Counter/>
+  
+  
+   
+ </Element>
+  </Container>)
+}
+const Container = styled.div`
+    width: 100%;
+    border: 1px solid #d1d8e4;
+`
+const Element = styled.div`
+  width: 400px;  
+  margin: 0 auto;
+   
+`
 export default App
